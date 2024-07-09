3552e764ac81ea27f4eda9c1e856faed449f5278
Harshit <raiharshit121@gmail.com>
2024-07-09 12:30:09 +0530

    L-12 drawing game score done

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ window.addEventListener('load',function() {
             this.speedX = Math.random() * -1.5 -0.5;
             this.markedForDeletion = false;
             this.lives = 5;
-            this.score - this.lives;
+            this.score = this.lives;
         }
         update(){
             this.x += this.speedX;
@@ -126,14 +126,23 @@ window.addEventListener('load',function() {
             this.game = game;
             this.fontSize = 25;
             this.fontFamily = 'Helvetica';
-            this.color = 'yellow';
+            this.color = 'white';
         }
         draw(context){
-            //ammo
+            context.save();
             context.fillStyle = this.color;
+            context.shadowOffsetX = 2;
+            context.shadowOffsetY = 2;
+            context.shadowColor = 'black';
+            context.font = this.fontSize + 'px ' + this.fontFamily;
+            //score
+            context.fillText('Score: '+ this.game.score, 20, 40);
+            //ammo
+
             for (let i = 0; i < this.game.ammo; i++){
                 context.fillRect(20 + 5 * i, 50, 3, 20);
             }
+            context.restore();
         }
     }
     class Game {
@@ -152,6 +161,8 @@ window.addEventListener('load',function() {
             this.ammoTimer = 0;
             this.ammoInterval = 500;
             this.gameOver = false;
+            this.score = 0;
+            this.winningScore = 10;
         }
         update(deltaTime) {
             this.player.update();
@@ -173,6 +184,7 @@ window.addEventListener('load',function() {
                         if(enemy.lives <= 0){
                             enemy.markedForDeletion = true;
                             this.score += enemy.score;
+                            if(this.score > this.winningScore) this.gameOver = true;
                         }
                     }
                 })
